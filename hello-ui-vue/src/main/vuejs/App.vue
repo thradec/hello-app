a07d8f80e82f513aaf90ba115422601191a25b53
Tomas Hradec <tomas.hradec@embedit.cz>
2017-01-26 15:46:48 +0100

    vue.js next iteration

---
+++ hello-ui-vue/src/main/vuejs/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div id="app">
+        <div class="container">
+            <div class="row">
+                <div class="col s6 offset-s3">
+                    <div class="card grey lighten-4">
+                        <div class="card-content">
+                            <h1 class="center-align" style="font-weight: 900">{{greeting}}</h1>
+                        </div>
+                        <div class="card-action">
+                            <a href="#" class="waves-effect waves-light btn blue" v-on:click="getGreeting">
+                                <i class="material-icons right">replay</i>next
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      greeting: "..."
+    };
+  },
+  methods: {
+    getGreeting: function (event) {
+      let self = this;
+      self.$http.get("/api/hello/random").then(function (response) {
+        self.greeting = response.data.message;
+      });
+    }
+  },
+  created: function () {
+    this.getGreeting();
+  }
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ hello-ui-vue/webpack.config.js
@@ -0,0 +1,64 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+    entry: './src/main/vuejs/main.js',
+    output: {
+        path: path.resolve(__dirname, './target/classes/static/dist'),
+        filename: '[name].js'
+    },
+    resolve: {
+        extensions: ['.js', '.vue'],
+        alias: {
+            'src': path.resolve(__dirname, '../src')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            }
+        ]
+    },
+    devServer: {
+        historyApiFallback: true,
+        noInfo: true
+    },
+    performance: {
+        hints: false
+    },
+    devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.devtool = '#source-map'
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        })
+    ])
+}
