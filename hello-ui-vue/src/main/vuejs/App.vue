cf9786e236e29e3f305d246c0c3f07f46c52f16f
Tomas Hradec <thradec@gmail.com>
2017-02-07 11:13:03 +0100

    vue.js use component

--- hello-ui-vue/src/main/vuejs/App.vue
+++ hello-ui-vue/src/main/vuejs/App.vue
@@ -1,48 +1,49 @@
 <template>
-    <div id="app">
-        <div class="container">
-            <div class="row">
-                <div class="col s6 offset-s3">
-                    <div class="card grey lighten-4">
-                        <div class="card-content">
-                            <h1 class="center-align" style="font-weight: 900">{{greeting}}</h1>
-                        </div>
-                        <div class="card-action">
-                            <a href="#" class="waves-effect waves-light btn blue" v-on:click="getGreeting">
-                                <i class="material-icons right">replay</i>next
-                            </a>
-                        </div>
+
+    <div id='app'>
+        <header>
+            <nav>
+                <div class='nav-wrapper grey darken-3'>
+                    <span id='app-logo'>
+                        <a href='#' class='app-name brand-logo'>
+                            <i class='material-icons'>code</i>hello-app
+                        </a>
+                    </span>
+                </div>
+            </nav>
+        </header>
+
+        <main>
+            <div class='container'>
+                <div class='row'>
+                    <div class='col s6 offset-s3'>
+                        <hello-card></hello-card>
                     </div>
                 </div>
             </div>
-        </div>
+        </main>
+
     </div>
+
 </template>
 
 <script>
 
+import HelloCard from './HelloCard.vue'
+
 export default {
-  name: 'app',
-  data() {
-    return {
-      greeting: "..."
-    };
-  },
-  methods: {
-    getGreeting: function (event) {
-      let self = this;
-      self.$http.get("/api/hello/random").then(function (response) {
-        self.greeting = response.data.message;
-      });
+    name: 'app',
+    components: {
+        'hello-card': HelloCard
     }
-  },
-  created: function () {
-    this.getGreeting();
-  }
 }
 
 </script>
 
 <style>
+
+#app-logo {
+    padding-left: 20px;
+}
 
 </style>
---
+++ hello-ui-vue/src/main/vuejs/HelloCard.vue
@@ -0,0 +1,38 @@
+<template>
+
+    <div class='card grey lighten-4'>
+        <div class='card-content'>
+            <h1 class='center-align' style='font-weight: 900'>{{greeting}}</h1>
+        </div>
+        <div class='card-action'>
+            <a href='#' class='waves-effect waves-light btn blue' v-on:click='getGreeting'>
+                <i class='material-icons right'>replay</i>next
+            </a>
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    name: 'hello-card',
+    data() {
+        return {
+            greeting: '...'
+        };
+    },
+    methods: {
+        getGreeting: function (event) {
+            let self = this;
+            self.$http.get('/api/hello/random').then(function (response) {
+                self.greeting = response.data.message;
+            });
+        }
+    },
+    created: function () {
+        this.getGreeting();
+    }
+}
+
+</script>
